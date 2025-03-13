369cad6a63b8ba544cf661f18dbdc12fdf3c16da
Catherine Vuthi <vuthi.esther@gmail.com>
2025-03-13 09:39:56 +0300

    Feat: filter by highlights functionality

--- frontend/src/app/components/filters/FilterByHighlights.tsx
+++ frontend/src/app/components/filters/FilterByHighlights.tsx
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Checkbox } from '@/components/ui/checkbox';
+import { HighlightFilters } from '@/types';
+
+type Props = {
+	onFilterChange: (filters: HighlightFilters) => void;
+};
 
 // Define the highlight options
 const highlightOptions = [
 	{ id: 'dealOfTheDay', label: 'Deal of the Day' },
 	{ id: 'freeShipping', label: 'Free Shipping' },
 	{ id: 'bestSellers', label: 'Best Sellers' },
-	{ id: 'featuredProducts', label: 'Featured Products' },
+	{ id: 'latest', label: 'Latest Products' },
 ];
 
-export const FilterByHighlights = () => {
+export const FilterByHighlights = ({ onFilterChange }: Props) => {
+	const [filters, setFilters] = useState<HighlightFilters>({
+		dealOfTheDay: false,
+		freeShipping: false,
+		bestSellers: false,
+		latest: false,
+	});
+
+	const handleCheckboxChange = (id: keyof HighlightFilters) => {
+		const newFilters = { ...filters, [id]: !filters[id] };
+		setFilters(newFilters);
+		onFilterChange(newFilters); // Pass the updated filters to the parent
+	};
+
 	return (
 		<div className="flex flex-col gap-3">
 			<p className="border-b border-grey pb-1 text-xs font-semibold">
@@ -20,7 +38,13 @@ export const FilterByHighlights = () => {
 					key={option.id}
 					className="flex items-center gap-2"
 				>
-					<Checkbox id={option.id} />
+					<Checkbox
+						id={option.id}
+						checked={filters[option.id as keyof HighlightFilters]}
+						onCheckedChange={() =>
+							handleCheckboxChange(option.id as keyof HighlightFilters)
+						}
+					/>
 					<label
 						htmlFor={option.id}
 						className="text-xs font-normal leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
